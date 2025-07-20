3a3ff9ae2a617fea4bebc813c96f64ec80fbcf21
unknown <ceo.wesleysantos@gmail.com>
2025-07-19 21:57:24 -0300

    Feita função basica no JS e estruturação final.

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+const produto1 = document.getElementById('produto1')
+const produto2= document.getElementById('produto2')
+const produto3 = document.getElementById('produto3')
+
+function excluirProduto(){
+    produto1.style = "display: none"
+}
+
+function excluirProduto2(){
+    produto2.style = "display: none"
+}
+
+function excluirProduto3(){
+    produto3.style = "display: none"
+}
